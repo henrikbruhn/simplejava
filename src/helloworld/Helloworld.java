1f164ba94a7a966547d5d229b6bc8aafb434d357
henrik <henrik@h3>
2019-08-06 12:52:26 +0200

    tilføjet NewClass

--- src/helloworld/Helloworld.java
+++ src/helloworld/Helloworld.java
@@ -17,6 +17,9 @@ public class Helloworld {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("blablablaaaa");
+        
+        NewClass obj = new NewClass("mere hallo");
+        System.out.println(obj);
     }
     
 }
